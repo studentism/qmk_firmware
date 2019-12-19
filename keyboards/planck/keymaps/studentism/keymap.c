d6b2e9a9c62303df5e10513be7631e8aa858bf8e
Jonathan Pierce <jonathan.leo.pierce@gmail.com>
2019-12-19 13:50:02 -0800

    Changed ESC/CTRL Holding ESC turns it into L_CTRL

--- keyboards/planck/keymaps/studentism/keymap.c
+++ keyboards/planck/keymaps/studentism/keymap.c
@@ -40,7 +40,7 @@ enum planck_keycodes {
   RAISE = LT(_RAISE, KC_SPC)
 };
 // see https://www.reddit.com/r/olkb/comments/4x3dei/hack_too_ugly_to_live/ for
-// information on the LOW/RAISE
+// information on the LOWER/RAISE
 //#define LOWER MO(_LOWER)
 //#define RAISE MO(_RAISE)
 
@@ -59,7 +59,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_QWERTY] = LAYOUT_planck_2x2u(
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
-    KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+    MT(MOD_LCTL, KC_ESC),  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT ,
     BACKLIT, KC_LCTL, KC_LALT, KC_LGUI, LOWER,  RAISE,  KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
 ),
